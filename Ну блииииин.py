a5494a166169ca8c93e387e04c60eac62ec93a08
elizvoron <91742422+elizvoron@users.noreply.github.com>
2021-10-19 20:53:54 +0400

    Update Ну блииииин.py
    Новый вариант)

--- Ну блииииин.py
+++ Ну блииииин.py
@@ -1,12 +1,22 @@
-gets = [', блин,', ', ну,', ', чтоб это ...,', ', твою на ..., ', 'ААА!']
-
-string = open('переменные.txt', encoding="utf-8", mode='r').read().split()
-if len(string) <= len(gets):
-    for i in range(len(string) - 1):
-        string[i] += gets[i]
-else:
-    for i in range(len(gets) - 1):
-        string[i] += gets[i]
+from dotenv import dotenv_values
+from random import uniform, randint
 
 
-print(*string)
+def blin(string, chance):
+    gets = [', блин,', ', ну,', ', чтоб это ...,', ', твою на ..., ', ', ААА']
+    if len(string) <= len(gets) / int(1 / chance):
+        for i in range(0, len(string) - 1, int(1 / chance)):
+            string[i] += gets[i]
+    else:
+        for i in range(0, len(string) - 1, int(1 / chance)):
+            chan = randint(0, len(gets) - 1)
+            string[i] += gets[chan]
+    return ' '.join(string)
+
+
+config = dotenv_values('.env')
+string11 = config['text']
+string1 = string11.split()
+chance = uniform(0, 1)
+print(chance)
+print(blin(string1, chance))
