18307ca6d80a8400995287d5ea4a18dd691d9e8b
elizvoron <91742422+elizvoron@users.noreply.github.com>
2021-10-19 20:54:50 +0400

    Update квадрат.py
    Не знаю, теперь похоже на квадратик))

--- квадрат.py
+++ квадрат.py
@@ -1,3 +1,14 @@
-x = int(open('переменные.txt', mode='r').read())
-for i in range(x):
-    print('*' * x)
+from dotenv import dotenv_values
+
+
+def rect(x):
+    rectt = []
+    for i in range(x):
+        rectt.append('* ' * x + '\n')
+    return rectt
+
+
+config = dotenv_values('.env')
+string_x = config['x']
+x = int(string_x)
+print(*rect(x))
