05b73957cfaf92f2bd82114500f193a30512c478
elizvoron <91742422+elizvoron@users.noreply.github.com>
2021-10-26 19:55:12 +0400

    Update квадрат.py

--- квадрат.py
+++ квадрат.py
@@ -3,12 +3,16 @@ from dotenv import dotenv_values
 
 def rect(x):
     rectt = []
-    for i in range(x):
-        rectt.append('* ' * x + '\n')
-    return rectt
+    if not x.isdigit():
+        raise ValueError('Неверный формат длинны стороны')
+    if x.isdigit() and int(x) <= 0:
+        raise ValueError('Длина стороны меньше 1')
+    if x.isdigit() and int(x) > 0:
+        for i in range(x):
+            rectt.append('*' * x + '\n')
+        return rectt
 
 
 config = dotenv_values('.env')
 string_x = config['x']
-x = int(string_x)
-print(*rect(x))
+print(*rect(string_x))
