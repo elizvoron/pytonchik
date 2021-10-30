bc96434fa81c537ddbe18144bad858ee067c1f58
elizvoron <91742422+elizvoron@users.noreply.github.com>
2021-10-30 19:40:02 +0400

    Add files via upload

--- gun.py
+++ gun.py
@@ -4,13 +4,44 @@ from random import randint
 def gun():
     a = randint(1, 6)
     if a < 5:
-        return 'ладно'
+        return 1
     else:
         raise ValueError
 
 
-while True:
+n1 = 0
+for i in range(300):
     try:
-        print(gun())
+        vv = gun()
     except ValueError:
-        print('Доигрался...')
+        vv = 0
+    if vv == 1:
+        n1 += 1
+print(n1 / 300)
+n1 = 0
+for i in range(3000):
+    try:
+        vv = gun()
+    except ValueError:
+        vv = 0
+    if vv == 1:
+        n1 += 1
+print(n1 / 3000)
+n1 = 0
+for i in range(30000):
+    try:
+        vv = gun()
+    except ValueError:
+        vv = 0
+    if vv == 1:
+        n1 += 1
+print(n1 / 30000)
+n1 = 0
+for i in range(300000):
+    try:
+        vv = gun()
+    except ValueError:
+        vv = 0
+    if vv == 1:
+        n1 += 1
+print(n1 / 300000)
